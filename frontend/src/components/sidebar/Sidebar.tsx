3a61f1cd9202e3dbb907dfd9e0f8760dd4024316
WPPManager Developer <pauloargolo87@gmail.com>
2026-05-02 18:51:25 -0300

    fix: importa useParams na Sidebar

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation, useParams } from 'react-router-dom';
 import { motion, AnimatePresence } from 'motion/react';
 import { 
   LayoutDashboard, 
